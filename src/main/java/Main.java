d5083dd90d16b035d4f513198ca2e9aa5d228c16
vinnipukh <vinnipukh@users.noreply.github.com>
2026-08-30 20:21:57 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,7 +29,7 @@ public class Main {
 
         ChatCompletion response = client.chat().completions().create(
                 ChatCompletionCreateParams.builder()
-                        .model("openrouter/free")
+                        .model("anthropic/claude-haiku-4.5")
                         .addUserMessage(prompt)
                         .build()
         );
