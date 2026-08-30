e466df8f54ddf52e0d1e916f7041af9ed7f6eb44
vinnipukh <vinnipukh@users.noreply.github.com>
2026-08-31 00:23:30 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -89,6 +89,5 @@ public class Main {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.err.println("Logs from your program will appear here!");
 
-        System.out.print(response.choices().get(0).message().content().orElse(""));
     }
 }
